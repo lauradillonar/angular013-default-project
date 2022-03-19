e0a276c62529d1d7847b8bc7a77d09ca5e7f9c9e
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-19 11:09:36 -0300

    Ejemplo de boton con Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Default Project';
 
-  website = {
-    name: 'Haz click para visitar Google',
-    href: 'https://www.google.com',
-    logo: 'https://cdn.iconscout.com/icon/free/png-256/google-152-189813.png',
-    description: "Uno de los buscadores más conocidos del mundo"
-  };
+  btnDisabled = true;
+  btnMsg = 'Botón deshabilitado';
 }
