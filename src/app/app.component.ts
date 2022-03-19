e4a840fd92fd4fd95b83dcbf92a48cbc5995d898
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-19 10:29:57 -0300

    Ejemplo con String Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular013-default-project';
+  title = 'Default Project';
+
+  website = {
+    name: 'Haz click para visitar Google',
+    href: 'https://www.google.com',
+    logo: 'https://cdn.iconscout.com/icon/free/png-256/google-152-189813.png',
+    description: "Uno de los buscadores más conocidos del mundo"
+  };
 }
