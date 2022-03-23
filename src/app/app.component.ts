3a5225f0c13418acce69e0b7888418b75a525ee1
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-23 09:06:45 -0300

    Ejemplo con Template View

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Default Project';
+  activateBtn = true;
 
-  logKey = (e: HTMLInputElement) => {
-      console.log(e.value);
+  writePassword = (pass: HTMLInputElement) => {
+    if (pass.value.length > 0){
+      this.activateBtn = false;
+      console.log(pass.value);
+    }else{
+      this.activateBtn=true;
+      console.log("Se ha ocultado el botón");
     }
   }
+
+  showPassword = (value: String) => {
+    alert(value);
+  }
+  
+  
+}
