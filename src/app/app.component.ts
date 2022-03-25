974012a83a15d87f9857fb52124c2584a52aeb67
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-25 02:53:44 -0300

    Ejemplo utilizando ngModel mediante Property binding y Event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,11 @@ export class AppComponent {
   nombre = 'David';
 
   printNombre(event: Event):void{
-    console.log('Valor inicial de nombre: '+this.nombre)
+    console.log('Valor inicial de nombre: '+this.nombre);
+    const evtar= event.target as HTMLInputElement; 
+    if (evtar !== null)
+      console.log('Valor que tenemos en nuestro evento: '+ evtar.value);
+      this.nombre = evtar.value;
+      console.log('Nuevo valor para nombre: '+this.nombre);
   }
 }
