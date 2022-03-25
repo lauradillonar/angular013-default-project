1df3f44dca82b7468c4c1628d03bb9004125e912
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-25 03:13:10 -0300

    Ejemplo utilizando ngModel bidireccional - Two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Default Project';
   nombre = 'David';
-
-  printNombre(event: Event):void{
-    console.log('Valor inicial de nombre: '+this.nombre);
-    const evtar= event.target as HTMLInputElement; 
-    if (evtar !== null)
-      console.log('Valor que tenemos en nuestro evento: '+ evtar.value);
-      this.nombre = evtar.value;
-      console.log('Nuevo valor para nombre: '+this.nombre);
-  }
 }
