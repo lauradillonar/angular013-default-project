d108e2e0b8c0093d0c6c5498539f5b0c96c6a3f8
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-26 19:49:46 -0300

    Ejemplo - Parte 2 - Usando operadores de seguridad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,6 @@ export class AppComponent {
     imgSrc: 'https://www.premiosgoya.com/wp-content/uploads/2015/12/Todo-sobre-mi-madre-320x432.jpg',
     director: 'Pedro Almodovar',
     anyo: 1999,
-    actors: [
-      {name: 'Cecilia Roth'},
-      {name: 'Marisa Paredes'},
-      {name: 'Candela Peña'},
-      {name: 'Antonia San Juan'},
-      {name: 'Penélope Cruz'}
-    ]
+    actors: null
   };
 }
